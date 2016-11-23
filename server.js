f33fab8b7538dc8d07b0702f740942f37cf33e0b
petermalcolm <peter.t.malcolm@gmail.com>
2016-11-22 21:03:53 -0500

    bring in ecstatic, http-server and html5 boilerplate

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 var RSVP = require('rsvp');
+// static file server using ecstatic
+var http = require('http')
+var ecstatic = require('ecstatic')
+var st = ecstatic(__dirname + '/public')
+
+var server = http.createServer(function (req, res) {
+  st(req, res)
+})
+server.listen(5000)
