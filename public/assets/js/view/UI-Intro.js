a22013bb60c9fc71ff87d536c5b4f7e90154fb14
petermalcolm <peter.t.malcolm@gmail.com>
2017-06-21 22:19:35 -0400

    intro placeholder titles

--- public/assets/js/view/UI-Intro.js
+++ public/assets/js/view/UI-Intro.js
@@ -6,8 +6,11 @@ const dgetID = document.getElementById.bind( document );
 
 ReactDOM.render(
 	RcE('div', { id: 'defyw' }, 
-		RcE('h3', { id: 'defyw-title' }, 'Define Your Way'),
-		RcE('div', { id: 'defyw-intro'}, 'Introductory info here ... ' )
+		RcE('h1', { id: 'defyw-title' }, 'Define Your Way'),
+		RcE('div', { id: 'defyw-intro'}, 'Introductory info here ... ' ),
+		RcE('h2', { id: 'defyw-new'}, 'Create a New Game' ),
+		RcE('h2', { id: 'defyw-or'}, '- OR -' ),
+		RcE('h2', { id: 'defyw-old'}, 'Join an Existing One' )
 	),
 	dgetID('root')
 );
