487f8a89e35a742d69804affa1336550127a278c
petermalcolm <peter.t.malcolm@gmail.com>
2017-08-04 22:23:22 -0400

    salt those passwords!

--- server-side/users.js
+++ server-side/users.js
@@ -1,8 +1,22 @@
 const md5 = require('md5');
+try {
+    var salt = require('../_salt.js');
+} catch (e) {
+    if (e.code == 'MODULE_NOT_FOUND') {
+    	console.log('Oh no!  You need a site-wide salt for your users\' passwords.');
+    	console.log('Just create a file called: \n');
+    	console.log('_salt.js\n');
+    	console.log('at the root level directory (next to server.js).');
+    	console.log('In case your feeling lazy, use this as the content of that file:\n');
+    	console.log('module.exports = \''+Math.random().toString(36).substring(16)+'\';\n');
+    	console.log('That salt was just created randomly, just for you.  Feel free to change it.');
+        process.exit();
+    }
+}
 
 const Users = function(db) {
 	const create = function(userInfo,callback) {
-		userInfo.password = md5(userInfo.password);
+		userInfo.password = md5(userInfo.password + salt);
 		db.put(userInfo.email,JSON.stringify(userInfo),function(err) {
 			return callback(err);
 		});
