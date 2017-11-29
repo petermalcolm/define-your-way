9edf7c818c6d65cad1dfeefc614f5614b29aa53a
petermalcolm <peter.t.malcolm@gmail.com>
2017-11-28 22:32:46 -0500

    first test/test.js - stubs for testing users.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,26 @@
-var assert = require('assert');
+var assert = require('chai').assert;
+var should = require('chai').should();
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+      [1,2,3].indexOf(4).should.equal(-1);
     });
   });
+});
+
+describe("Users Mechanisms", function() {
+	describe("Create a User", function() {
+		it('should not insert a user to the db that already exists', function() {
+			// TODO: real code here ...
+			['foobar'].toString().should.equal(['foobar'].toString());
+		});
+		it('should insert a user to the db that does not exist yet', function() {
+			// TODO: real code here ...
+		});
+	});
+
+	describe("Authenticate a User", function() {
+			// TODO: real code here ...
+	});
 });
