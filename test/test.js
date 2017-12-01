2129b2d1c85ca304be5c392e038bf3b3ff70a9cb
petermalcolm <peter.t.malcolm@gmail.com>
2017-11-30 22:44:40 -0500

    first real test (new user)
    TODO: deal with unhandled Promise rejection

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ chai.should();
 
 // A DB for users, games and cached API responses
 const levelup = require('level');
-const dbNoPromise = levelup('./define-db');
+const dbNoPromise = levelup('../define-db');
 
 // Some node magic for making Promises:
 const {promisify} = require('util');
@@ -22,6 +22,22 @@ const userlib = require('../server-side/users.js');
 const users = new userlib(db);
 const gamelib = require('../server-side/games.js');
 const games = new gamelib(db); 
+
+// and a password that is not on github:
+try {
+    var testCreds = require('./_test-creds.js');
+} catch (e) {
+    if (e.code == 'MODULE_NOT_FOUND') {
+    	console.log('Oh no!  You need credentials for test users.');
+    	console.log('Just create a file called: \n');
+    	console.log('_test-creds.js\n');
+    	console.log('in the test/ directory (next to test.js).');
+    	console.log('In case your feeling lazy, use this as the content of that file:\n');
+    	console.log('module.exports = \''+Math.random().toString(36).substring(2)+'\';\n');
+    	console.log('That password was just created randomly, just for you.  Feel free to change it.');
+        process.exit();
+    }
+} 
 
 ///////////////////
 //   THE TESTS   //
@@ -38,8 +54,11 @@ describe('Array', function() {
 describe("Users Mechanisms", function() {
 	describe("Create a User", function() {
 		it('should not insert a user to the db that already exists', function() {
-			// TODO: real code here ...
-			['foobar'].toString().should.equal(['foobar'].toString());
+		expect( users.create({	name : 'John Doe',
+								email : 'johndoe@example.com',
+								password : testCreds,
+								avatar : 'http://example.com/avatar.png'
+							}) ).to.eventually.be.a('string');
 		});
 		it('should insert a user to the db that does not exist yet', function() {
 			// TODO: real code here ...
